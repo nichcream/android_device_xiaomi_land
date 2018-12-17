422a2ab743a40105ce72b282443c61b84c7ee48e
Nich <nctrenco@gmail.com>
2018-12-18 00:55:56 +0800

    land: Copy QDCM configuration files
    These were accidently removed when creating msm8937-common.
    
    Change-Id: I49573dd44fe2f2302e9e3ddf66a85c27ece2c811

--- device.mk
+++ device.mk
@@ -32,6 +32,12 @@ PRODUCT_PACKAGES += \
     libmmjpeg_interface \
     libqomx_core
 
+# Display
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/qdcm_calib_data_hx8394F_HD720p_video_BOE.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_hx8394F_HD720p_video_BOE.xml \
+    $(LOCAL_PATH)/configs/qdcm_calib_data_ili9881c_HD720p_video_Tianma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_ili9881c_HD720p_video_Tianma.xml \
+    $(LOCAL_PATH)/configs/qdcm_calib_data_nt35521s_HD720p_video_EBBG.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_nt35521s_HD720p_video_EBBG.xml
+
 # Init scripts ramdisk
 PRODUCT_PACKAGES += \
     init.device.rc
