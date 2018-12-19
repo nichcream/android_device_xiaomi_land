c57374eeb017f6a81415df24afb815377adc50af
Nich <nctrenco@gmail.com>
2018-12-19 20:19:22 +0800

    land: Build fingerprint 2.0 HAL
    Xiaomi 8937 devices now have to specify the make target for the
    fingerprint HAL.
    
    Change-Id: Id08f900445252d3bbd27061e26afb7bbb122edf1

--- device.mk
+++ device.mk
@@ -38,6 +38,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qdcm_calib_data_ili9881c_HD720p_video_Tianma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_ili9881c_HD720p_video_Tianma.xml \
     $(LOCAL_PATH)/configs/qdcm_calib_data_nt35521s_HD720p_video_EBBG.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_nt35521s_HD720p_video_EBBG.xml
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.0-service.custom
+
 # Init scripts ramdisk
 PRODUCT_PACKAGES += \
     init.device.rc
